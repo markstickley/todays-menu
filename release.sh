9f932265c1c0184027871b70ff07f15f8400540e
Mark Stickley <markstickley@theyakshack.co.uk>
2025-06-12 22:03:44 +0100

    Updated release script to add a tag to github

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script builds and pushes Docker images for the Today's Menu application.
+# This script builds and pushes Docker images for the Today's Menu application, and adds a github tag for the specified version.
 # It requires a version argument to tag the image appropriately.
 # It is intended to be run from the root of the repository.
 # It should not be run in a container, but rather on a host machine with Docker installed.
@@ -16,3 +16,7 @@ docker build -t markstickley/todays-menu:latest --platform linux/amd64 .
 docker build -t markstickley/todays-menu:"$VERSION" --platform linux/amd64 .
 docker push markstickley/todays-menu:latest
 docker push markstickley/todays-menu:"$VERSION"
+
+# Add a git tag for the version
+git tag -a "$VERSION" -m "Release version $VERSION"
+git push origin "$VERSION"
