01f5c23a006899f70a1bfae62a1fd190a97f0689
Mark Stickley <markstickley@theyakshack.co.uk>
2025-06-12 21:17:55 +0100

    Added release script

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script builds and pushes Docker images for the Today's Menu application.
+# It requires a version argument to tag the image appropriately.
+# It is intended to be run from the root of the repository.
+# It should not be run in a container, but rather on a host machine with Docker installed.
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+VERSION="$1"
+
+docker build -t markstickley/todays-menu:latest --platform linux/amd64 .
+docker build -t markstickley/todays-menu:"$VERSION" --platform linux/amd64 .
+docker push markstickley/todays-menu:latest
+docker push markstickley/todays-menu:"$VERSION"
