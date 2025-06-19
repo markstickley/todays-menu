6e9677feb68b202f1eaba83c6a23b77c623e4ae1
Mark Stickley <markstickley@theyakshack.co.uk>
2025-06-19 20:25:46 +0100

    Safer way of exporting environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Export environment variables for cron jobs in a shell-compatible way
-printenv | grep -v "no_proxy" | awk -F= '{gsub("'\"'", "\\\"", $2); print "export "$1"=\""$2"\""}' > /etc/profile.d/envvars.sh
+( printenv | grep -v "^no_proxy=" | while IFS='=' read -r name value; do
+    [ -z "$name" ] && continue
+    esc_value=$(printf '%s' "$value" | sed 's/\\/\\\\/g; s/\"/\\\"/g')
+    printf 'export %s="%s"\n' "$name" "$esc_value"
+done ) > /etc/profile.d/envvars.sh
 
 # Run cron in background
 cron
